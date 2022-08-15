c0e845f657d0146a3804c284a8682b76f3e01562
linleckkyalsin <51205058+linleckkyalsin@users.noreply.github.com>
2022-08-15 10:33:17 +0630

    Create github-actions-scripts.sh

---
+++ github-actions-scripts.sh
@@ -0,0 +1,9 @@
+ls -a
+          echo is Java Installed?
+          java -version
+          echo is Git installed?
+          git --version
+          echo what about bild tools?
+          mvn --version
+          gradle --version
+          ant -version
